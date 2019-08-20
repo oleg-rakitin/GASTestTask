7d2818bfd8956d55709ad340a5fa7da62a462896
limonchik16@gmail.com <9elzqcqv9i3m>
2019-08-20 19:01:57 +0300

    Replace String with ENUM

--- src/test/java/ru/gasis/MockitoTests.java
+++ src/test/java/ru/gasis/MockitoTests.java
@@ -14,6 +14,14 @@ import static org.mockito.Mockito.*;
 
 public class MockitoTests {
 
+    public enum TypeOfInputParameter {
+        FIRST_NAME_EMPTY,
+        LAST_NAME_EMPTY,
+        FIRST_NAME_NULL,
+        LAST_NAME_NULL,
+        BOTH
+    }
+
     UserAccountDAO userAccountDAO;
     UserAccount userAccount;
 
@@ -35,69 +43,69 @@ public class MockitoTests {
 
     @Test(expectedExceptions = NullPointerException.class,description = "Throws an NullPointerException if the parameter passed is null")
     public void testGetUserAccountWithNullArgument() throws SQLException, GasIsUserNotFoundException {
-        testGetUserAccountWithIncorrectParameters("firstNameNull");
+        testGetUserAccountWithIncorrectParameters(TypeOfInputParameter.FIRST_NAME_NULL);
     }
 
     @Test(expectedExceptions = IllegalArgumentException.class,description = "Throws an IllegalArgumentException if the parameter passed is empty")
     public void testGetUserAccountWithEmptyArgument() throws SQLException, GasIsUserNotFoundException {
-        testGetUserAccountWithIncorrectParameters("firstNameEmpty");
+        testGetUserAccountWithIncorrectParameters(TypeOfInputParameter.FIRST_NAME_EMPTY);
     }
 
     @Test(expectedExceptions = GasIsUserNotFoundException.class, description = "Throws an " +
             "GasIsUserNotFoundException if an account with the same name is not found.")
      public void testChangeUserLastNameWithInvalidArguments() throws SQLException, GasIsUserNotFoundException {
-        testChangeUserLastNameThrowException("both");
+        testChangeUserLastNameThrowException(TypeOfInputParameter.BOTH);
     }
 
     @Test(expectedExceptions = IllegalArgumentException.class, description = "Throws an IllegalArgumentException if the first name is entered as an empty string")
     public void testChangeUserLastNameWithInvalidUserNameArgument() throws SQLException, GasIsUserNotFoundException {
-        testChangeUserLastNameThrowException("firstNameEmpty");
+        testChangeUserLastNameThrowException(TypeOfInputParameter.FIRST_NAME_EMPTY);
     }
 
     @Test(expectedExceptions = IllegalArgumentException.class, description = "Throws an IllegalArgumentException if the last name is entered as an empty string")
     public void testChangeUserLastNameWithInvalidLastNameArgument() throws SQLException, GasIsUserNotFoundException {
-        testChangeUserLastNameThrowException("lastNameEmpty");
+        testChangeUserLastNameThrowException(TypeOfInputParameter.LAST_NAME_EMPTY);
     }
 
     @Test(expectedExceptions = NullPointerException.class, description = "Throws an NullPointerException if the first name is entered as an null object")
     public void testChangeUserLastNameWithFirstNameNullArgument() throws SQLException, GasIsUserNotFoundException {
-        testChangeUserLastNameThrowException("firstNameNull");
+        testChangeUserLastNameThrowException(TypeOfInputParameter.FIRST_NAME_NULL);
     }
 
     @Test(expectedExceptions = NullPointerException.class, description = "Throws an NullPointerException if the last name is entered as an null object")
     public void testChangeUserLastNameWithLastNameNullArgument() throws SQLException, GasIsUserNotFoundException {
-        testChangeUserLastNameThrowException("lastNameNull");
+        testChangeUserLastNameThrowException(TypeOfInputParameter.LAST_NAME_NULL);
     }
 
 
-    private void testGetUserAccountWithIncorrectParameters(String type) throws SQLException, GasIsUserNotFoundException {
+    private void testGetUserAccountWithIncorrectParameters(TypeOfInputParameter type) throws SQLException, GasIsUserNotFoundException {
         switch (type) {
-            case "firstNameNull":
+            case FIRST_NAME_NULL:
                 doThrow(NullPointerException.class).when(userAccountDAO).getUserAccount(isNull());
                 userAccountDAO.getUserAccount(null);
                 break;
-            case "firstNameEmpty":
+            case FIRST_NAME_EMPTY:
                 doThrow(IllegalArgumentException.class).when(userAccountDAO).getUserAccount(any());
                 userAccountDAO.getUserAccount("");
                 break;
         }
     }
 
-    private void testChangeUserLastNameThrowException(String type) throws SQLException, GasIsUserNotFoundException {
+    private void testChangeUserLastNameThrowException(TypeOfInputParameter type) throws SQLException, GasIsUserNotFoundException {
         switch (type) {
-            case "firstNameEmpty":
+            case FIRST_NAME_EMPTY:
                 doThrow(IllegalArgumentException.class).when(userAccountDAO).changeUserLastName((String) any(),any());
                 userAccountDAO.changeUserLastName("", "notExist");
                 break;
-            case "lastNameEmpty":
+            case LAST_NAME_EMPTY:
                 doThrow(IllegalArgumentException.class).when(userAccountDAO).changeUserLastName((String) any(),any());
                 userAccountDAO.changeUserLastName("notExist", "");
                 break;
-            case "firstNameNull":
+            case FIRST_NAME_NULL:
                 doThrow(NullPointerException.class).when(userAccountDAO).changeUserLastName((UserAccount) isNull(),any());
                 userAccountDAO.changeUserLastName((UserAccount) null, "notExist");
                 break;
-            case "lastNameNull":
+            case LAST_NAME_NULL:
                 doThrow(NullPointerException.class).when(userAccountDAO).changeUserLastName((UserAccount) any(),isNull());
                 userAccountDAO.changeUserLastName(userAccount, null);
                 break;
